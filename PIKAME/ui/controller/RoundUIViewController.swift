15b865de9dfe991018f90e3be4f047262ce4671b
Sorax5 <sorax5.r23@gmail.com>
2025-04-04 00:07:20 +0530

    add rounded & background color

--- PIKAME/ui/controller/RoundUIViewController.swift
+++ PIKAME/ui/controller/RoundUIViewController.swift
@@ -21,25 +21,7 @@ extension UIView {
         }
         set {
             layer.cornerRadius = newValue
-            layer.masksToBounds = newValue > 0
-        }
-    }
-    
-    @IBInspectable var borderWidthV: CGFloat {
-        get {
-            return layer.borderWidth
-        }
-        set {
-            layer.borderWidth = newValue
-        }
-    }
-    
-    @IBInspectable var borderColorV: UIColor? {
-        get {
-            return UIColor(cgColor: layer.borderColor!)
-        }
-        set {
-            layer.borderColor = newValue?.cgColor
+            layer.masksToBounds = true
         }
     }
 }
